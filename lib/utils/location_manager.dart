ffa2c27a859e943ff6e9c9fe996bf6e33d26b1c0
Tukaram <tukaram.n@reapmind.com>
2025-07-11 19:16:23 +0530

    add location permission done

--- lib/utils/location_manager.dart
+++ lib/utils/location_manager.dart
@@ -1,75 +1,87 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:geolocator/geolocator.dart' as geo;
+import 'package:permission_handler/permission_handler.dart' as perm;
+import 'package:atd/main.dart'; // <-- Add this import at the top if not present
 
-class LocationServiceManager {
-  static StreamSubscription<ServiceStatus>? _subscription;
-
-  static void startMonitoring(BuildContext context) {
-    // Cancel any previous subscription
-    _subscription?.cancel();
-
-    _subscription = Geolocator.getServiceStatusStream().listen((status) {
-      if (status == ServiceStatus.disabled) {
-        _showLocationDialog(context);
-      }
-    });
-  }
-
-  static void stopMonitoring() {
-    _subscription?.cancel();
-  }
-
-  static void _showLocationDialog(BuildContext context) {
-    final navigator = Navigator.of(context, rootNavigator: true);
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (context) => AlertDialog(
-        title: const Text("Location Required"),
-        content: const Text("Please enable your location to continue using the app."),
-        actions: [
-          TextButton(
-            onPressed: () async {
-              navigator.pop(); // Close dialog
-              await Geolocator.openLocationSettings();
-            },
-            child: const Text("Open Settings"),
-          ),
-        ],
-      ),
-    );
-  }
-}
 class LocationAwareWrapper extends StatefulWidget {
   final Widget child;
-
   const LocationAwareWrapper({super.key, required this.child});
 
   @override
   State<LocationAwareWrapper> createState() => _LocationAwareWrapperState();
 }
 
-class _LocationAwareWrapperState extends State<LocationAwareWrapper>
-    with WidgetsBindingObserver {
+class _LocationAwareWrapperState extends State<LocationAwareWrapper> {
+  StreamSubscription<geo.ServiceStatus>? _serviceStatusStream;
+  bool _dialogOpen = false;
+
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addObserver(this);
+    _checkLocationPermission();
+    _listenToServiceStatus();
+  }
 
-    // Check on first frame
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      LocationServiceManager.startMonitoring(context);
+  Future<void> _checkLocationPermission() async {
+    final permission = await perm.Permission.location.request();
+    final serviceEnabled = await geo.Geolocator.isLocationServiceEnabled();
+
+    if (permission.isGranted && !serviceEnabled && !_dialogOpen) {
+      _showLocationDialog();
+    }
+  }
+
+  void _showLocationDialog() {
+    _dialogOpen = true;
+    showDialog(
+      context: navigatorKey.currentContext!, // Use global navigatorKey context
+      barrierDismissible: false,
+      builder: (_) => AlertDialog(
+        title: const Text("Location Required"),
+        content: const Text("Please turn on location services."),
+        actions: [
+          TextButton(
+            onPressed: () async {
+              await geo.Geolocator.openLocationSettings();
+            },
+            child: const Text("Turn On Location"),
+          ),
+        ],
+      ),
+    ).then((_) {
+      if (mounted) {
+        setState(() {
+          _dialogOpen = false;
+        });
+      } else {
+        _dialogOpen = false;
+      }
+    });
+  }
+
+  void _listenToServiceStatus() {
+    _serviceStatusStream =
+        geo.Geolocator.getServiceStatusStream().listen((status) {
+      if (status == geo.ServiceStatus.disabled && !_dialogOpen) {
+        _showLocationDialog();
+      } else if (status == geo.ServiceStatus.enabled && _dialogOpen) {
+        if (navigatorKey.currentState?.canPop() ?? false) {
+          navigatorKey.currentState?.pop();
+        }
+        // _dialogOpen will be reset in .then() of showDialog
+      }
     });
   }
 
   @override
   void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    LocationServiceManager.stopMonitoring();
+    _serviceStatusStream?.cancel();
     super.dispose();
   }
 
   @override
-  Widget build(BuildContext context) => widget.child;
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
 }
